8aa883702a61715acba2889dbc5caf4b398794aa
Huy Phatt <phatle219@gmail.com>
2025-10-03 22:49:45 +0700

    feat: create ui help-center

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,11 @@ $('.product-list').slick({
   cssEase: 'linear',
    slidesToScroll: 1,
 });
+$('.slider-faq-box').slick({
+  slidesToShow:4,
+  dots: false,
+  infinite: false,
+  speed: 500,
+  cssEase: 'linear',
+  slidesToScroll: 1,
+});
