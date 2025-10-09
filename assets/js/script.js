4bc0cf768c1447d74001666cb1f7e9b5138db789
text <leanhphq@gmail.com>
2025-10-09 23:55:39 +0700

    push code len nhanh search

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,14 @@
 //     cssEase:'linear',
 //     infinite:true,
 // });
+
+$(document).ready(function(){
+  $('.product-slider').slick({
+    slidesToShow: 5,    // Số sản phẩm hiển thị cùng lúc
+    slidesToScroll: 3,  // Số sản phẩm trượt mỗi lần click
+    autoplay: true,     // Tự động trượt
+    autoplaySpeed: 2000, // Tốc độ trượt (2 giây)
+    dots: true,         // Hiển thị dấu chấm điều hướng
+    infinite: true      // Lặp vô hạn
+  });
+});
