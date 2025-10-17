cad25f0e095283a16c605136a90478fef921dee0
Huy Phatt <phatle219@gmail.com>
2025-10-17 21:46:55 +0700

    feat: reponsive - .faq-form-tag - .faq-contain p - .faq-contain-padding - .slider-faq-box

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,36 @@ $('.slider-faq-box').slick({
   speed: 500,
   cssEase: 'linear',
   slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+       slidesToShow:3,
+        dots: true,
+        autoplay: true,     
+        autoplaySpeed: 2000,  
+        speed: 800, 
+      }
+    },
+     {
+      breakpoint: 767,
+      settings: {
+       slidesToShow:2,
+        dots: true,
+        autoplay: true, 
+        autoplaySpeed: 2000,
+        speed: 800,
+      }
+    },
+    {
+      breakpoint: 473,
+      settings: {
+       slidesToShow:1,
+        dots: true,
+        autoplay: true, 
+        autoplaySpeed: 2000,
+        speed: 800,
+      }
+    },
+  ]
 });
