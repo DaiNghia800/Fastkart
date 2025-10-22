1a4e929ac35663853665e1dd8c02efd54a557088
Huy Phatt <phatle219@gmail.com>
2025-10-22 14:06:32 +0700

    feat: reponsive - right-box-contain - product-list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,27 @@ $('.product-lists').slick({
   speed: 500,
   cssEase: 'linear',
    slidesToScroll: 1,
+   responsive:[
+    {
+      breakpoint: 599,
+      settings: {
+       slidesToShow:2,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+       slidesToShow:3,
+        dots: true,
+      }
+    },
+     {
+      breakpoint: 1200,
+      settings: {
+       slidesToShow:4,
+        dots: true,
+      }
+    },
+   ]
 });
