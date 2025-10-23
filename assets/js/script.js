01e08e3a943aff41e9213bfc354abf4a9f4329f7
text <leanhphq@gmail.com>
2025-10-23 14:31:15 +0700

    responsive search

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,33 @@ $(document).ready(function(){
     autoplay: true,     // Tự động trượt
     autoplaySpeed: 2000, // Tốc độ trượt (2 giây)
     dots: true,         // Hiển thị dấu chấm điều hướng
-    infinite: true      // Lặp vô hạn
+    infinite: true,      // Lặp vô hạn
+
+    // Thêm phần responsive
+    responsive: [
+      {
+        breakpoint: 1200, // Màn hình từ 768px đến 992px
+        settings: {
+          slidesToShow: 4,   // 3 cái
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 992, // Màn hình từ 768px đến 992px
+        settings: {
+          slidesToShow: 3,   // 3 cái
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768, // Màn hình nhỏ hơn 768px
+        settings: {
+          slidesToShow: 2,   // 2 cái
+          slidesToScroll: 1
+        }
+      }
+      // Bạn có thể thêm các breakpoint khác ở đây
+    ]
   });
 });
 // End Search
