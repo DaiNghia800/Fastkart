f7e19cc949897ac3ac3181204448a0864683e14f
Huy Phatt <phatle219@gmail.com>
2025-10-26 12:51:12 +0700

    repush help-center

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,46 @@ $('.product-lists').slick({
     },
    ]
 });
+$('.slider-faq-box').slick({
+  slidesToShow:4,
+  dots: false,
+  infinite: false,
+  speed: 500,
+  cssEase: 'linear',
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+       slidesToShow:3,
+        dots: true,
+        autoplay: true,     
+        autoplaySpeed: 2000,  
+        speed: 800, 
+      }
+    },
+     {
+      breakpoint: 767,
+      settings: {
+       slidesToShow:2,
+        dots: true,
+        autoplay: true, 
+        autoplaySpeed: 2000,
+        speed: 800,
+      }
+    },
+    {
+      breakpoint: 473,
+      settings: {
+       slidesToShow:1,
+        dots: true,
+        autoplay: true, 
+        autoplaySpeed: 2000,
+        speed: 800,
+      }
+    },
+  ]
+});
 $('.three-slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
